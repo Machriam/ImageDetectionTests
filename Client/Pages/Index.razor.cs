44c44eb43fe8375efc7173faac754133a2004ef4
Amadeus Lehmann <no@mail.com>
2023-10-26 18:31:50 +0200

    works as intended

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,35 @@
+using global::Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using OpenCvSharp;
+using SpawnDev.BlazorJS.JSObjects;
+
+namespace ImageDetectionTests.Client.Pages
+{
+    public partial class Index
+    {
+        private ElementReference canvasSrcRef;
+        private ElementReference canvasDestRef;
+
+        public async Task OnFileChanged(InputFileChangeEventArgs args)
+        {
+            using var memoryStream = new MemoryStream();
+            await args.File.OpenReadStream().CopyToAsync(memoryStream);
+            var image = "data:image/png;base64," + Convert.ToBase64String(memoryStream.ToArray());
+            await UpdateImage(image);
+        }
+
+        protected async Task UpdateImage(string image)
+        {
+            using var canvasSrcEl = new HTMLCanvasElement(canvasSrcRef);
+            using var canvasSrcCtx = canvasSrcEl.Get2DContext();
+            using var canvasDestEl = new HTMLCanvasElement(canvasDestRef);
+            using var canvasDestCtx = canvasDestEl.Get2DContext();
+            using var src = new Mat();
+            await src.LoadImageURL(image);
+            src.DrawOnCanvas(canvasSrcCtx, true);
+            using var dst = new Mat();
+            Cv2.Canny(src, dst, 50, 200);
+            dst.DrawOnCanvas(canvasDestCtx, true);
+        }
+    }
+}
