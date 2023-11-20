464fd91ac9108149b423038d3456c87c54d524ae
Amadeus Lehmann <no@mail.com>
2023-11-20 18:42:13 +0100

    removed spawndev dependency

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,7 +2,6 @@ using ImageDetectionTests.Client;
 using ImageDetectionTests.Client.Core;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SpawnDev.BlazorJS;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IImageDataHandler, ImageDataHandler>();
 builder.Services.AddTransient<IOpenCvInterop, OpenCvInterop>();
-builder.Services.AddBlazorJSRuntime();
 
 await builder.Build().RunAsync();
