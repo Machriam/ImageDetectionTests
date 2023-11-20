4c40ece3f4f60b06805d64c0253377a188753c92
Amadeus Lehmann <no@mail.com>
2023-11-20 22:10:27 +0100

    removed willReadFrequently warning. But it is not faster this way

--- Client/Components/Image.razor.js
+++ Client/Components/Image.razor.js
@@ -1,4 +1,5 @@
 export function DrawSourceImage(guid, data) {
+    document.getElementById(guid).getContext("2d", { willReadFrequently: true });
     const canvas = document.getElementById(guid);
     const image = document.createElement("img");
     image.src = data;
@@ -8,18 +9,32 @@ export function DrawSourceImage(guid, data) {
     };
 }
 export function Canny(sourceGuid, destGuid, params) {
+    InvokeStep(sourceGuid, destGuid, (src, dest) => {
+        cv.Canny(src, dest, params[0], params[1], 3, false);
+    });
+}
+
+export function MedianBlur(sourceGuid, destGuid, params) {
+    InvokeStep(sourceGuid, destGuid, (src, dest) => {
+        cv.medianBlur(src, dest, params[0]);
+    });
+}
+
+function InvokeStep(sourceGuid, destGuid, modifyImage) {
+    document.getElementById(destGuid).getContext("2d", { willReadFrequently: true });
     const src = cv.imread(sourceGuid);
     const dest = new cv.Mat();
-    cv.Canny(src, dest, params[0], params[1], 3, false);
+    modifyImage(src, dest);
     cv.imshow(destGuid, dest);
     src.delete();
     dest.delete();
 }
-export function MedianBlur(sourceGuid, destGuid, params) {
-    const src = cv.imread(sourceGuid);
-    const dest = new cv.Mat();
-    cv.medianBlur(src, dest, params[0]);
-    cv.imshow(destGuid, dest);
-    src.delete();
-    dest.delete();
+function _measureTime(name, func) {
+    const start = Date.now();
+    func();
+    const end = Date.now();
+    if (window.state == undefined) window.state = {};
+    if (!window.state.hasOwnProperty(name)) window.state[name] = 0;
+    state[name] = state[name] + end - start;
+    console.log(name + " " + (state[name]));
 }
